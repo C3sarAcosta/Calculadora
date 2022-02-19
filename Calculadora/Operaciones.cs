758d2466ad63d4a45b6fc23411bcc077cecaf11a
César Acosta <c3sar.adan@gmail.com>
2022-02-19 11:37:50 -0700

    Agregar archivos de proyecto.

---
+++ Calculadora/Operaciones.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Operaciones
+    {
+        /* *, /, +, -, sqrt, ^ */
+        public double resultado { get; set; }
+
+        public Operaciones()
+        {
+            this.resultado = 0;
+        }
+        
+        public double Operacion(double valor1)
+        {
+            resultado = Math.Sqrt(valor1);
+            return resultado;
+        }
+
+        public double Operacion(double numero1, double numero2, string operacion)
+        {
+            switch (operacion)
+            {
+                case "suma":
+                    resultado = numero1 + numero2;
+                    break;
+                case "resta":
+                    resultado = numero1 - numero2;
+                    break;
+                case "multiplicacion":
+                    resultado = numero1 * numero2;
+                    break;
+                case "division":
+                    if (numero2 != 0)
+                    {
+                        resultado = numero1 / numero2;
+                    }
+                    else
+                    {
+                        resultado = 0;
+                    }
+                    break;
+                case "ptencia":
+                    resultado = Math.Pow(numero1, numero2);
+                    break;
+                default:
+                    resultado = 0;
+                    break;
+            }
+            return resultado;
+        }
+
+        public double Operacion(double numero1, double numero2, double numero3, string operacion)
+        {
+            switch(operacion)
+            {
+                case "suma":
+                    resultado = numero1 + numero2 + numero3;
+                    break;
+                case "resta":
+                    resultado = numero1 - numero2 - numero3;
+                    break;
+                case "multiplicacion":
+                    resultado = numero1 * numero2 * numero3;
+                    break;
+                default:
+                    resultado= 0;
+                    break;
+            }
+            return resultado;
+        }
+    }
+}
---
+++ Calculadora/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        /* *, /, +, -, sqrt, ^ */
+        static void Main(string[] args)
+        {
+            string op = "suma";
+            Operaciones operacion = new Operaciones();
+            while (op != "salir")
+            {
+                
+                Console.WriteLine("Cuanto valores vas a ingresar");
+                int valores = Convert.ToInt32(Console.ReadLine());
+                if(valores == 1)
+                {
+                    Console.WriteLine("Ingresa el valor");
+                    double valor1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine(" La raiz cuadrada de " + valor1 + " = " +operacion.Operacion(valor1));
+                }
+                else if(valores == 2)
+                {
+                    Console.WriteLine("Ingresa el valor 1");
+                    double valor1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Ingresa el valor 2");
+                    double valor2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Ingresa la operacion suma, resta, multiplicacion, division, potencia");
+                    op = Console.ReadLine();
+                    Console.WriteLine(" La " + op + " de " + valor1 + " y " + valor2 + " = " + operacion.Operacion(valor1, valor2, op));
+                }
+            }
+        }
+    }
+}
